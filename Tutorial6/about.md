062c62adda99b16cccdca3ca82e5546450287db8
CK Ng <79681367+ckng0221@users.noreply.github.com>
2021-04-24 18:34:21 +0800

    Add files via upload

---
+++ Tutorial6/about.md
@@ -0,0 +1,37 @@
+#### About this BMI Calculator
+
+This is an app trial created for WQD7001 Principle of Data Science For Tutorial 6 for practice purpose. <br>
+
+Name : <b>Ng Choon Khon</b><br>
+ID : <b>S2028941</b>
+
+<br><br>
+
+
+#### What is BMI?
+
+**Body Mass Index (BMI)** is essentially a value obtained from the weight and height of a person. <br><br>
+
+
+#### Calculating the BMI
+BMI can be computed by dividing the person's weight (kg) by their squared height (m) as follows:
+
+> BMI = kg/m^2
+
+where *kg* represents the person's weight and *m^2* the person's squared height. <br><br>
+
+
+
+
+
+#### BMI Check
+
+To check your BMI Weight Status:
+
+Below 18.5 -> Underweight
+
+18.5 - 24.9 -> Normal
+
+25.0 - 29.9 -> Overweight
+
+30.0 - Above -> Obsese
---
+++ Tutorial6/app.R
@@ -0,0 +1,100 @@
+library(shiny)
+library(shinythemes)
+
+
+# =========== User Interface ============= #
+
+ui <- fluidPage(theme = shinytheme("superhero"),
+                navbarPage("BMI Calculator (Tutorial 6):",
+                           tabPanel("Home",
+                                    # Input values
+                                    sidebarPanel(
+                                      HTML("<h3>Input parameters</h3>"),
+                                      sliderInput("height", 
+                                                  label = "Height (cm)", 
+                                                  value = 175, 
+                                                  min = 40, 
+                                                  max = 250),
+                                      sliderInput("weight", 
+                                                  label = "Weight (kg)", 
+                                                  value = 70, 
+                                                  min = 20, 
+                                                  max = 100),
+                                      
+                                      actionButton("submitbutton", 
+                                                   "Submit", 
+                                                   class = "btn btn-primary")
+                                    ),
+                                    
+                                    mainPanel(
+                                      tags$label(h3('Status/Output')), # Status/Output Text Box
+                                      verbatimTextOutput('contents'),
+                                      tableOutput('tabledata') # Results table
+                                    ) # mainPanel()
+                                    
+                           ), #tabPanel(), Home
+                           
+                           tabPanel("About", 
+                                    titlePanel("About"), 
+                                    div(includeMarkdown("about.md"), 
+                                        align="justify")
+                           ) #tabPanel(), About
+                           
+                ) # navbarPage()
+) # fluidPage()
+
+
+
+
+# ======== Server ===========================
+server <- function(input, output, session) {
+  
+  # Input Data
+  datasetInput <- reactive({  
+    
+    # formula for bmi 
+    bmi <<- input$weight/( (input$height/100) * (input$height/100) )
+    bmi_df <- data.frame(bmi)
+    names(bmi_df) <- "BMI"
+    print(bmi_df)
+    
+    # print(bmi_status)
+    
+  })
+  
+  # Status/Output Text Box
+  output$contents <- renderPrint({
+    
+    #bmi weight status check
+    bmi_status_func <- function(bmi_value) {
+      if (bmi_value < 18.5) {
+        return("Underweight")
+      } else if (bmi_value >= 18.5 & bmi_value < 25) {
+        return("Normal")
+      } else if (bmi_value >= 25 & bmi_value < 30 ) {
+        return("Overweight")
+      } else if (bmi_value >= 30) {
+        return("Obsese")
+      }
+    }
+    
+    if (input$submitbutton>0) { 
+      isolate(bmi_status_func(bmi_value=bmi)) 
+    } else {
+      return("Please enter your weight and heights to calculate the BMI")
+    }
+  })
+  
+  # Prediction results table
+  output$tabledata <- renderTable({
+    if (input$submitbutton>0) { 
+      isolate(datasetInput()) 
+    } 
+  })
+  
+}
+
+
+
+# Create Shiny Ap
+shinyApp(ui = ui, server = server)
